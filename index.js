7c89d199048c329e9606de86bbf207fad3cc8475
Alejandra Paez <alepaezdev@gmail.com>
2023-06-05 20:46:39 -0600

    feat: leer archivo endpoint

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const server = express();
+const fsPromises = require("fs/promises");
 
 /*
 * app de express -> 
@@ -9,10 +10,21 @@ const server = express();
 *   2. request
 */
 
+
 server.get("/", (request, response) => {
   response.write("Estamos en HOME");
   response.end()
 })
+
+/**
+ * arrow funtcions -> async () => {}
+ * async function ()
+ */
+// Hacer un endpoint que lea un archivo y nos lo responda
+server.get("/archivo", async (request, response) => {
+  const data = await fsPromises.readFile("./archivoALeer.txt", "utf-8")
+  response.send(data);
+});
 
 server.listen(8080, () => {
   console.log("Nuestro servidor esta prendido!!");
